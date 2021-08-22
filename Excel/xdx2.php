422a26aacffe1fc61cca03b8536b5fded5518ff7
JDeat-S <jhovan14000@gmail.com>
2021-08-22 01:26:37 -0500

    Envio de correos con PHP
    Ya envia correos con formato falta que verifique la lic su correo

---
+++ Excel/xdx2.php
@@ -0,0 +1,14 @@
+<html>
+    <body>
+   <h1 class="formuh1">Enviar.</h1>  
+
+   <form id="form1" class="well col-lg-12" action="xd.php" method="post" name="form1" enctype="multipart/form-data">
+          <label>archivo</label>
+          <input type="file" name="adjunto" id="archivo-adjunto">
+
+        <button type="submit">Enviar</button>
+    </form>
+
+
+  </body>
+</html>
