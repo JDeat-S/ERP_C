5a1ea15a6856f5419e78e44a6e20297f59f8449e
JDeat-S <jhovan14000@gmail.com>
2021-09-08 12:36:03 -0500

    Colores tabla general.
    ahora muestra colores dependiendo del status

--- src/Funciones/ColorFilas.java
+++ src/Funciones/ColorFilas.java
@@ -10,33 +10,42 @@ import java.awt.Component;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableCellRenderer;
 
-
 /**
  *
  * @author JDeat
  */
 public class ColorFilas extends DefaultTableCellRenderer {
-    Color RyB=new Color(255, 175, 175);
+
+    Color RyB = new Color(255, 175, 175);
+    Color imss = new Color(203, 0, 255);
+    Color Pen = new Color(146, 199, 255);
+    Color EEs = new Color(152, 244, 147);
 
     @Override
-    
-    public Component getTableCellRendererComponent(JTable table, Object value, 
-                                                   boolean isSelected, 
-                                                   boolean hasFocus, 
-                                                   int row, 
-                                                   int column) {
+
+    public Component getTableCellRendererComponent(JTable table, Object value,
+            boolean isSelected,
+            boolean hasFocus,
+            int row,
+            int column) {
         super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
-        if(table.getValueAt(row, 20).toString().equals("BAJA")){
+        if (table.getValueAt(row, 20).toString().equals("BAJA")) {
             setBackground(RyB);
-        } else if (table.getValueAt(row, 20).toString().equals("VIGENTE")){
+        } else if (table.getValueAt(row, 20).toString().equals("VIGENTE")) {
             setBackground(Color.yellow);
-        } else if (table.getValueAt(row, 20).toString().equals("RECHAZADO")){
+        } else if (table.getValueAt(row, 20).toString().equals("RECHAZADO")) {
             setBackground(RyB);
-        } 
- 
+        } else if (table.getValueAt(row, 20).toString().equals("IMSS")) {
+            setBackground(imss);
+        } else if (table.getValueAt(row, 20).toString().equals("EN ESPERA")) {
+            setBackground(EEs);
+        } else if (table.getValueAt(row, 20).toString().equals("PENDIENTE")) {
+            setBackground(Pen);
+        } else{
+            setBackground(Color.CYAN);
+        }
+
         return this;
- 
-    
 
     }
 }
