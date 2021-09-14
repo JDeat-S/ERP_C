2715e3ae38d9e55605a5114c0ad3c508ee6b54ff
Jhovan Alexandro Gallardo Torres <jhovan14000@gmail.com>
2021-09-14 16:56:13 -0500

    Update ColorZyS.java

--- src/ColoresT/ColorZyS.java
+++ src/ColoresT/ColorZyS.java
@@ -15,14 +15,15 @@ import javax.swing.table.DefaultTableCellRenderer;
  * @author JD
  */
 public class ColorZyS extends DefaultTableCellRenderer {
-    Color S1 = new Color(0, 255, 0);
 
-    Color Zn = new Color(255, 80, 80);
+    Color S1 = new Color(0, 255, 0);
+    Color FT = new Color(255, 80, 80);
     Color S2 = new Color(183, 244, 99);
     Color FA = new Color(248, 193, 253);
     Color ZP = new Color(20, 255, 206);
     Color CPC = new Color(255, 201, 100);
     Color FP = new Color(150, 114, 84);
+    Color Zn = new Color(203, 132, 255);
 
     @Override
 
@@ -49,7 +50,7 @@ public class ColorZyS extends DefaultTableCellRenderer {
         } else if (table.getValueAt(row, 2).toString().equals("Foraneos Acapulco")) {
             setBackground(FA);
         } else if (table.getValueAt(row, 2).toString().equals("Foraneos Toluca")) {
-            setBackground(FA);
+            setBackground(FT);
         } else {
             setBackground(Color.GRAY);
         }
