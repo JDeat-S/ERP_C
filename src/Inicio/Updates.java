522c21dfe176aaf1e855985f8822e30c573d740d
Jhovan Alexandro Gallardo Torres <jhovan14000@gmail.com>
2021-09-14 17:37:09 -0500

    UPDATE

--- src/Inicio/Updates.java
+++ src/Inicio/Updates.java
@@ -222,7 +222,7 @@ public class Updates extends javax.swing.JFrame {
                     //5 segundos de busqueda
                     Thread.sleep(5000);
                     if (verificarConexion()) {
-                        if (version().equals("1.5.5")) {
+                        if (version().equals("1.5.6")) {
                             LabelAll.setText("La aplicacion esta actualizada.");
                             btnDesc.setVisible(false);
                         } else {
