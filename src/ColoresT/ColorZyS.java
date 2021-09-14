2afccbf45c009fc0b38478d0e67bf9121f1a2ea2
Jhovan Alexandro Gallardo Torres <jhovan14000@gmail.com>
2021-09-14 16:49:37 -0500

    Update ColorZyS.java

--- src/ColoresT/ColorZyS.java
+++ src/ColoresT/ColorZyS.java
@@ -15,13 +15,14 @@ import javax.swing.table.DefaultTableCellRenderer;
  * @author JD
  */
 public class ColorZyS extends DefaultTableCellRenderer {
+    Color S1 = new Color(0, 255, 0);
 
-    Color SAN = new Color(203, 132, 255);
-    Color Evn = new Color(183, 244, 99);
-    Color SVP = new Color(248, 193, 253);
-    Color EstCDMX = new Color(20, 255, 216);
+    Color Zn = new Color(255, 80, 80);
+    Color S2 = new Color(183, 244, 99);
+    Color FA = new Color(248, 193, 253);
+    Color ZP = new Color(20, 255, 206);
     Color CPC = new Color(255, 201, 100);
-    Color LD = new Color(77, 255, 0);
+    Color FP = new Color(150, 114, 84);
 
     @Override
 
@@ -31,22 +32,24 @@ public class ColorZyS extends DefaultTableCellRenderer {
             int row,
             int column) {
         super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
-        if (table.getValueAt(row, 11).toString().equals("SANTANDER CDMX")) {
-            setBackground(SAN);
-        } else if (table.getValueAt(row, 13).toString().equals("Cerrado por Contingencia")) {
+        if (table.getValueAt(row, 13).toString().equals("Cerrado por Contingencia")) {
             setBackground(CPC);
-        } else if (table.getValueAt(row, 11).toString().equals("SANTANDER FORANEOS")) {
-            setBackground(SAN);
-        } else if (table.getValueAt(row, 11).toString().equals("EVENTOS")) {
-            setBackground(Evn);
-        } else if (table.getValueAt(row, 11).toString().equals("SERVICIOS VP")) {
-            setBackground(SVP);
-        } else if (table.getValueAt(row, 11).toString().equals("ESTACIONAMIENTOS FORANEOS")) {
-            setBackground(EstCDMX);
-        } else if (table.getValueAt(row, 11).toString().equals("ESTACIONAMIENTOS CDMX")) {
-            setBackground(EstCDMX);
-        } else if (table.getValueAt(row, 11).toString().equals("VARIADOS")) {
-            setBackground(Color.yellow);
+        } else if (table.getValueAt(row, 2).toString().equals("Sur 1")) {
+            setBackground(S1);
+        } else if (table.getValueAt(row, 2).toString().equals("Norte")) {
+            setBackground(Zn);
+        } else if (table.getValueAt(row, 2).toString().equals("Foraneos Puebla")) {
+            setBackground(FP);
+        } else if (table.getValueAt(row, 2).toString().equals("Sur 2")) {
+            setBackground(S2);
+        } else if (table.getValueAt(row, 2).toString().equals("Poniente")) {
+            setBackground(ZP);
+        } else if (table.getValueAt(row, 2).toString().equals("Oficina")) {
+            setBackground(Color.YELLOW);
+        } else if (table.getValueAt(row, 2).toString().equals("Foraneos Acapulco")) {
+            setBackground(FA);
+        } else if (table.getValueAt(row, 2).toString().equals("Foraneos Toluca")) {
+            setBackground(FA);
         } else {
             setBackground(Color.GRAY);
         }
