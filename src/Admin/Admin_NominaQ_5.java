616bb5fa138bf3d06e9b5395ae8f6aaad1507d14
Jhovan Alexandro Gallardo Torres <jhovan14000@gmail.com>
2021-09-24 08:39:06 -0500

    Ver 1.6.1

--- src/Admin/Admin_NominaQ_5.java
+++ src/Admin/Admin_NominaQ_5.java
@@ -664,7 +664,8 @@ public final class Admin_NominaQ_5 extends javax.swing.JFrame {
         double d24 = Double.parseDouble(this.Q24.getText());
         double d25 = Double.parseDouble(this.Iad.getText());
 
-        double total = d25 / (d1 + d2 + d3 + d4 + d5 + d6 + d7 + d8 + d9 + d10 + d11 + d12 + d13 + d14 + d15 + d16 + d17 + d18 + d19 + d20 + d21 + d22 + d23 + d24);
+        double total = d25 / (d1 + d2 + d3 + d4 + d5 + d6 + d7 + d8 + d9 + d10 + d11 + d12 + d13 
+                + d14 + d15 + d16 + d17 + d18 + d19 + d20 + d21 + d22 + d23 + d24);
         this.PQT.setText("" + total + "");
     }
 
