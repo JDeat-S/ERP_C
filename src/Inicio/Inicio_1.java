98ba08ec900cef6a60358fe64e4fc719315cc032
Jhovan Alexandro Gallardo Torres <jhovan14000@gmail.com>
2021-11-30 13:39:13 -0600

    1.16 b 2

--- src/Inicio/Inicio_1.java
+++ src/Inicio/Inicio_1.java
@@ -150,10 +150,8 @@ public class Inicio_1 extends javax.swing.JFrame {
         //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new Inicio_1().setVisible(true);
-            }
+        java.awt.EventQueue.invokeLater(() -> {
+            new Inicio_1().setVisible(true);
         });
     }
 
